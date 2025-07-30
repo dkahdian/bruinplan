df3c7be3cef76b5792c6eaa184a5564694ee32c1
David Kahdian <dkahdian@g.ucla.edu>
2025-07-29 19:26:20 -0700

    Attempt 1 to fix new path error

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ export default {
       precompress: false
     }),
     paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+      base: process.env.BASE_PATH
     }
   }
 };
